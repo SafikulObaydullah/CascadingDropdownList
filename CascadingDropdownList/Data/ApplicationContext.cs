23e353c44f19e1fa0037b844a82a4d436440c2c5
obaydullah <obaydullah599@gmail.com>
2022-06-17 16:47:57 +0600

    Ajax DropdownList

---
+++ CascadingDropdownList/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using CascadingDropdownList.Models.Cascade;
+using Microsoft.EntityFrameworkCore;
+
+namespace CascadingDropdownList.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : 
+            base(options) 
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }   
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ CascadingDropdownList/Models/Cascade/Country.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CascadingDropdownList.Models.Cascade
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
